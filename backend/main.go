36b3d7855dedb139152daa965a02ef8e84492c0c
Laith Assaf <laitho4325@gmail.com>
2025-03-04 02:10:12 -0500

    Initial commit: Full-stack chat application with Go backend and React frontend

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Novapool/chat-app/pkg/websocket"
+	"github.com/google/uuid"
+)
+
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		ID:   uuid.New().String(),
+		Conn: conn,
+		Pool: pool,
+		Send: make(chan []byte),
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Distributed Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
